9914618e65b843128e6f340058bd1a8e9cdedfde
mmickey7552-cpu <mmickey7552@gmail.com>
2026-09-17 13:32:40 +0530

    Update test.py
    fixed indentation

--- test.py
+++ test.py
@@ -11,17 +11,17 @@ class TestModel(unittest.TestCase):
         from tensorflow.keras.models import load_model
         self.restored_model=load_model(self.top_model_complete)
 
-def convert_img(self, img):
-    import numpy as np
-    from tensorflow.keras.utils import load_img, img_to_array
-    from tensorflow.keras import applications
-
-    sample_image = load_img(img, target_size=(150, 150))
-    sample_img = img_to_array(sample_image) / 255.0          # (150, 150, 3)
-    sample = np.expand_dims(sample_img, axis=0)              # (1, 150, 150, 3)
-    self.base_model = applications.VGG16(include_top=False, weights='imagenet',
-                                         input_shape=(150, 150, 3))
-    return self.base_model.predict(sample)                   # (1, 4, 4, 512)
+    def convert_img(self, img):
+        import numpy as np
+        from tensorflow.keras.utils import load_img, img_to_array
+        from tensorflow.keras import applications
+    
+        sample_image = load_img(img, target_size=(150, 150))
+        sample_img = img_to_array(sample_image) / 255.0          # (150, 150, 3)
+        sample = np.expand_dims(sample_img, axis=0)              # (1, 150, 150, 3)
+        self.base_model = applications.VGG16(include_top=False, weights='imagenet',
+                                             input_shape=(150, 150, 3))
+        return self.base_model.predict(sample)                   # (1, 4, 4, 512)
 
     def test_sample(self):
         sample1 = self.samples_path+"/sample1.jpg"
